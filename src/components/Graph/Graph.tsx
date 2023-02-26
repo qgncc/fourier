18373020a5152e791cc4783ba96c157bb50c9caa
0xEVG <evgenikrolikov@gmail.com>
2023-02-26 21:05:07 +0700

    fixed Graph. Now function draws from correct starting point

--- src/components/Graph/Graph.tsx
+++ src/components/Graph/Graph.tsx
@@ -16,23 +16,27 @@ type Coords = [number, number]
 
 export function Graph({scale = 1, mathFunction, width, height, detailLevel = 1}: GraphProps){
     
-    const xViewBox = 500
-    const yViewBox = 500
+    const xViewBox = width
+    const yViewBox = height
     
-    const coordsStartX = 250
-    const coordsStartY = xViewBox/2
+    const centerX = width/2
+    const centerY = height/2
+
 
     const unitInterval = 1*scale
 
     function centerCoordsToSVGCoords([x,y]:Coords):Coords {
-      return [x+250, -y+250]
+      return [x+centerX, -y+centerY]
     }
 
     function createLine(func: MathFunction) {
-      let line = ""
-      for (let x = (-xViewBox/2)/unitInterval; x < (xViewBox/2)/unitInterval; x+=1/detailLevel) {
-        let [SVGX, SVGY] = centerCoordsToSVGCoords([x*unitInterval, func(x)*unitInterval])
+      let startX = -centerX/unitInterval
+      let endX = centerX/unitInterval
+      
+      let line = `M ${centerCoordsToSVGCoords([startX*unitInterval, func(startX)*unitInterval]).join(" ")} `
 
+      for (let x = startX; x < endX; x+=1/detailLevel) {
+        let [SVGX, SVGY] = centerCoordsToSVGCoords([x*unitInterval, func(x)*unitInterval])
         line += `L ${SVGX} ${SVGY} `
       }
       return line
@@ -42,9 +46,9 @@ export function Graph({scale = 1, mathFunction, width, height, detailLevel = 1}:
 
     return( 
       <svg width={width} height={height} viewBox={`0 0 ${xViewBox} ${yViewBox}`}>
-        <path d={`M ${coordsStartX} 0 L ${coordsStartX} ${yViewBox}`} fill="transparent" stroke="#312312"/>
-        <path d={`M 0 ${coordsStartY} L ${xViewBox} ${coordsStartY}`} fill="transparent" stroke="#312312"/>
-        <path d={`M 0 ${coordsStartY}`+ line}  fill="transparent" stroke="#ff0000"/>
+        <path d={`M ${centerX} 0 L ${centerX} ${yViewBox}`} fill="transparent" stroke="#312312"/>
+        <path d={`M 0 ${centerY} L ${xViewBox} ${centerY}`} fill="transparent" stroke="#312312"/>
+        <path d={line}  fill="transparent" stroke="#ff0000"/>
       </svg>
       )
 }
