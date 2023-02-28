6b63bb2657abde660aeed23ba837b8cb4280d0b8
0xEVG <evgenikrolikov@gmail.com>
2023-02-28 15:23:55 +0700

    Prettier

--- src/components/Graph/Graph.tsx
+++ src/components/Graph/Graph.tsx
@@ -1,56 +1,71 @@
-import { useMemo } from "react"
+import { useMemo } from "react";
 
-type Points = Array<[number, number]>
-type MathFunction = (x: number)=>number 
+type Points = Array<[number, number]>;
+type MathFunction = (x: number) => number;
 
-interface GraphProps{
-    mathFunction: MathFunction,
-    width: number,
-    height: number,
-    scale?: number,
-    detailLevel?: number
+interface GraphProps {
+  mathFunction: MathFunction;
+  width: number;
+  height: number;
+  scale?: number;
+  detailLevel?: number;
 }
 
-type Coords = [number, number]
+type Coords = [number, number];
 
+export function Graph({
+  scale = 1,
+  mathFunction,
+  width,
+  height,
+  detailLevel = 1,
+}: GraphProps) {
+  const xViewBox = width;
+  const yViewBox = height;
 
+  const centerX = width / 2;
+  const centerY = height / 2;
 
+  const unitInterval = 1 * scale;
 
-export function Graph({scale = 1, mathFunction, width, height, detailLevel = 1}: GraphProps){
-    
-    const xViewBox = width
-    const yViewBox = height
-    
-    const centerX = width/2
-    const centerY = height/2
+  function centerCoordsToSVGCoords([x, y]: Coords): Coords {
+    return [x + centerX, -y + centerY];
+  }
 
+  function createLine(func: MathFunction) {
+    let startX = -centerX / unitInterval;
+    let endX = centerX / unitInterval;
 
-    const unitInterval = 1*scale
+    let line = `M ${centerCoordsToSVGCoords([
+      startX * unitInterval,
+      func(startX) * unitInterval,
+    ]).join(" ")} `;
 
-    function centerCoordsToSVGCoords([x,y]:Coords):Coords {
-      return [x+centerX, -y+centerY]
+    for (let x = startX; x < endX; x += 1 / detailLevel) {
+      let [SVGX, SVGY] = centerCoordsToSVGCoords([
+        x * unitInterval,
+        func(x) * unitInterval,
+      ]);
+      line += `L ${SVGX} ${SVGY} `;
     }
+    return line;
+  }
 
-    function createLine(func: MathFunction) {
-      let startX = -centerX/unitInterval
-      let endX = centerX/unitInterval
-      
-      let line = `M ${centerCoordsToSVGCoords([startX*unitInterval, func(startX)*unitInterval]).join(" ")} `
+  let line = useMemo(() => createLine(mathFunction), [mathFunction]);
 
-      for (let x = startX; x < endX; x+=1/detailLevel) {
-        let [SVGX, SVGY] = centerCoordsToSVGCoords([x*unitInterval, func(x)*unitInterval])
-        line += `L ${SVGX} ${SVGY} `
-      }
-      return line
-    }
-
-    let line = useMemo(()=>createLine(mathFunction), [mathFunction])  
-
-    return( 
-      <svg width={width} height={height} viewBox={`0 0 ${xViewBox} ${yViewBox}`}>
-        <path d={`M ${centerX} 0 L ${centerX} ${yViewBox}`} fill="transparent" stroke="#312312"/>
-        <path d={`M 0 ${centerY} L ${xViewBox} ${centerY}`} fill="transparent" stroke="#312312"/>
-        <path d={line}  fill="transparent" stroke="#ff0000"/>
-      </svg>
-      )
+  return (
+    <svg width={width} height={height} viewBox={`0 0 ${xViewBox} ${yViewBox}`}>
+      <path
+        d={`M ${centerX} 0 L ${centerX} ${yViewBox}`}
+        fill="transparent"
+        stroke="#312312"
+      />
+      <path
+        d={`M 0 ${centerY} L ${xViewBox} ${centerY}`}
+        fill="transparent"
+        stroke="#312312"
+      />
+      <path d={line} fill="transparent" stroke="#ff0000" />
+    </svg>
+  );
 }
